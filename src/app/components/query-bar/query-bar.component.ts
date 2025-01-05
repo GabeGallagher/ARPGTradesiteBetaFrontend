1b78070a0dd0a569332cc4eb3154625d43b5e280
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2025-01-05 15:52:53 -0600

    made changes to query bar to take user input

--- src/app/components/query-bar/query-bar.component.ts
+++ src/app/components/query-bar/query-bar.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ItemQuery } from '../../interfaces/item.interface';
 
 @Component({
   selector: 'app-query-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './query-bar.component.html',
-  styleUrl: './query-bar.component.css'
+  styleUrl: './query-bar.component.css',
 })
-export class QueryBarComponent {
 
+export class QueryBarComponent {
+  query: ItemQuery = {};
+
+  @Output() searchSubmitted = new EventEmitter<ItemQuery>();
+
+  itemTypes = [
+    'Any',
+    'Weapon',
+    'Helm',
+    'Chest',
+    'Belt',
+    'Ring',
+    'Gloves',
+    'Boots',
+    'Off-Hand',
+    'Amulet',
+  ];
+  rarities = ['Any', 'Normal', 'Uncommon', 'Rare', 'Legendary'];
+  datesPosted = ['Any', 'Last Hour', '24 Hours', 'Last Week'];
+
+  onSearch() {
+    const searchQuery = Object.fromEntries(
+      Object.entries(this.query).filter(
+        ([_, value]) => value !== '' && value != null
+      )
+    );
+    this.searchSubmitted.emit(searchQuery);
+    console.log(`Searching query: ${searchQuery.toString()}`);
+  }
 }
--- src/app/components/trade/trade.component.ts
+++ src/app/components/trade/trade.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { QueryBarComponent } from "../query-bar/query-bar.component";
+import { QueryBarComponent } from '../query-bar/query-bar.component';
+import { ItemQuery } from '../../interfaces/item.interface';
 
 @Component({
   selector: 'app-trade',
   standalone: true,
   imports: [QueryBarComponent],
   templateUrl: './trade.component.html',
-  styleUrl: './trade.component.css'
+  styleUrl: './trade.component.css',
 })
 export class TradeComponent {
-
+  handleSearch(query: ItemQuery) {
+    console.log(`Heard item query in trade component: ${query}`);
+  }
 }
